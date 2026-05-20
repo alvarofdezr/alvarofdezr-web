586571e1b83df91ecb6229226c9d3d22f5d97a2e
alvarofdezr <alvarofdezr@outlook.es>
2026-05-20 19:01:13 +0200

    feat: added profile to the web, fixed Footer and Header to have the correct info, added buttom to go to the main page in all pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Álvaro Fernández - Cybersecurity & Software Engineering';
+export const SITE_TITLE = 'Álvaro Fernández Ramos';
 export const SITE_DESCRIPTION = 'Portfolio and technical blog of a Computer Engineering student focused on cybersecurity, secure software development, and offensive security research.';
